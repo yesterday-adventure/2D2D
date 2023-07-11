81e591f771ee5a6d7d3f1d0f92e033c02296109d
lIo0O0oIl <tjddms0923tjddms@gmail.com>
2023-07-11 20:15:14 +0900

    PlayerPrefabs 코드 제작
    - 게임을 제일 처음 재생 시에만 인트로무비(스토리)가 재생되도록 플레이어프리텝을 사용해서 코드 제작
    - 이후 애니메이션 만들기

---
+++ GhostSteal/Assets/02.Scripts/SE/IntroMovie.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IntroMovie : MonoBehaviour
+{
+    [SerializeField]
+    private bool init = false;
+
+    private void Awake()
+    {
+        if (!PlayerPrefs.HasKey("introMovie")/* || PlayerPrefs.GetInt("introMovie") == 0*/)
+        {
+            // 처음 시작한 경우
+            Debug.Log("처음 시작한 것이므로 인트로무비 영상 재생.");
+            // 인트로무비 영상 재생하기
+
+            PlayerPrefs.SetInt("introMovie", 1);
+        }
+    }
+
+    private void Update()
+    {
+        if (init)
+        {
+            //PlayerPrefs.SetInt("introMovie", 0);
+            //PlayerPrefs.DeleteKey("introMovie");
+            PlayerPrefs.DeleteAll();
+            //Debug.Log("다시 확인용으로 초기화 해줌");
+            Debug.Log("다시 확인용으로 데이터 지워줌. 다시 플레이 시 인트로무비 재생됨.");
+            init = false;
+        }
+    }
+}
