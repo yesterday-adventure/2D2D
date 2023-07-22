16b8ad95a80fd520a630d4a58e0cefe59836fe14
준이 <98876187+June060522@users.noreply.github.com>
2023-07-22 20:08:02 +0900

    test

--- GhostSteal/Assets/02.Scripts/tjfdk/Rayder.cs
+++ GhostSteal/Assets/02.Scripts/tjfdk/Rayder.cs
@@ -4,17 +4,20 @@ using UnityEngine;
 
 public class Rayder : MonoBehaviour
 {
-    private void OnTriggerEnter2D(Collider2D other) {
+    private void OnTriggerStay2D(Collider2D other) {
         
         if(other.CompareTag("Player"))
         {
-            if (other.transform.childCount != 0) {
+            if (other.transform.childCount != 1) {
 
-                Destroy(other.transform.GetChild(0).gameObject);
-                other.transform.GetComponent<SpriteRenderer>().enabled = true;
-                other.transform.GetComponent<PolygonCollider2D>().enabled = true;
+                Destroy(other.transform.GetChild(1).gameObject);
+                other.transform.GetComponentInChildren<SpriteRenderer>().enabled = true;
+                other.transform.GetComponent<BoxCollider2D>().enabled = true;
             }
             Move.Instance.curItem = null;
+            Move.Instance.pot = false;
+            GameManager.Instance.isPot = false;
+            GameManager.Instance.isLight = true;
         }
     }
 }
